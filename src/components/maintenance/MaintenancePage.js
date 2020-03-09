fa09af60cb0cdc4054473899a3cc89e477074713
ajhudson <andyhudson1979@gmail.com>
2020-03-09 21:25:38 +0000

    check in to save work

--- src/components/maintenance/MaintenancePage.js
+++ src/components/maintenance/MaintenancePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const MaintenancePage = () => {
+const MaintenancePage = ({caption}) => {
     return (
         <div>
             <h3>Sorry - We're undergoing Maintenance at the moment.</h3>
+    <h4>Maintenance type: {caption}</h4>
             <hr />
         </div>
     )
--- src/components/maintenance/MaintenancePageHandler.js
+++ src/components/maintenance/MaintenancePageHandler.js
@@ -3,8 +3,16 @@ import { useFlags } from 'launchdarkly-react-client-sdk';
 import MaintenancePage from './MaintenancePage';
 
 const MaintenancePageHandler = ({children}) => {
-    const { fullSiteMaintenance } = useFlags();
-    const toRender = fullSiteMaintenance ? <MaintenancePage /> : children;
+    const { fullSiteMaintenance, partialSiteMaintenance } = useFlags();
+    let caption = "";
+
+    if (fullSiteMaintenance) {
+        caption = 'full site maintenance';
+    } else if (partialSiteMaintenance) {
+        caption = 'partial site maintenance'
+    }
+
+    const toRender = fullSiteMaintenance || partialSiteMaintenance ? <MaintenancePage caption={caption} /> : children;
 
     return (
         <>{ toRender }</>
